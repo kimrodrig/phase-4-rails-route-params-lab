e57570dce3d5d17b4934ffb485c373a63cd72151
Kim <rodrigkim@gmail.com>
2022-11-01 13:58:46 -0400

    commit tree 1b2ed190fde16d37b02142b20b826aa83cc607c7

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,8 +1,16 @@
 class StudentsController < ApplicationController
 
   def index
-    students = Student.all
+    if params["name"]
+      students = Student.find_by(first_name: params["name"].titleize) ||  Student.find_by(last_name: params["name"].titleize)
+    else
+      students = Student.all
+    end
     render json: students
+  end
+
+  def show
+    render json: Student.find(params[:id])
   end
 
 end
